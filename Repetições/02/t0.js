cdf6ed940b07e1da46617326bb2c2f6be516fa1e
thomxz-cod <thomazerick35@gmail.com>
2025-09-07 17:27:12 -0300

    Fix event listener assignment and improve element selection

--- Repetições/02/t0.js
+++ Repetições/02/t0.js
@@ -1,7 +1,7 @@
-var click = document.addEventListener('click', clicar)
+var div_C = document.getElementById('res1') 
+var click = res1.addEventListener('click', clicar)
 
 function clicar() {
-    var res = document.querySelector('div#res')
     var res1 = document.getElementById('res1')
     var cem = Number(100)
     var r = (`${cem+1}, ${cem+2}, ${cem+3}, ${cem+4}, ${cem+5}, ${cem+6}, ${cem+7}, ${cem+8}, ${cem+9}, ${cem+10},`)
